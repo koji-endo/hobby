c570eb2f7e5f12c7750572d1a976b739cc5571d4
Koji Endo <skyclear.288@gmail.com>
2018-07-10 09:02:44 +0900

    C csv読み込み

---
+++ GA/reader.c
@@ -0,0 +1,18 @@
+#include "reader.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(){
+    FILE *fp;
+    char *filename="connect_sei0530.txt";
+    char readline[256];
+    while(fgets(readline,256,fp)!=NULL){
+         if(readline[0]!='#'){
+strok(readline," ");
+ 
+}
+    }
+
+}
